300e7a7a4fe70b908ff8fc9f4c9759619200976f
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2025-03-14 19:08:44 -0500

    introducción a python

---
+++ uso_de_python.py
@@ -0,0 +1,37 @@
+# Autor: Andres Salazar
+# Este es un comentario de una sola linea
+'''
+
+este es un comentario de
+múltiples
+lineas
+
+'''
+print("Estudiando python")
+
+#Definición de variables
+'''
+reglas:
+Python es sensible a minusculas y mayúsculas
+No se pueden usar espacios
+No se pueden usar palabras reservadas
+No es conveniente usar números como nombres de variables
+'''
+x=50
+y=15
+
+print(x+y)
+
+print(type(x))
+# <class 'int'> : significa que x es un número entero
+nombre="Andres"
+print(nombre)
+print(type(nombre))
+#<class 'str'> : significa que la variable nombre es un String (cadena)
+
+print(type(x))
+x=4.8
+print(type(x))
+#<class 'float'> : significa que la variable es un flotante (número decimal)
+
+print(x , nombre)
